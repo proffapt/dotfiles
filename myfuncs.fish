ac4cb4d9d9ed133db188f78317de22d2e03a5c7d
proffapt <proffapt@pm.me>
2022-08-27 04:05:04 +0530

    updating via gsync-syncscript by @proffapt

--- myfuncs.fish
+++ myfuncs.fish
@@ -1,3 +1,7 @@
 function gc --description "Cloning github directories"
 	git clone https://github.com/$argv
 end
+
+function gt --description "Moving to directories seamlesly"
+	cd $(autojump $argv)
+end
