a968b1bc86d13103e2265c1024a3f94170d14e32
proffapt <proffapt@pm.me>
2022-10-10 13:11:51 +0530

    update

--- myfuncs.fish
+++ myfuncs.fish
@@ -5,3 +5,11 @@ end
 function gt --description "Moving to directories seamlesly"
 	cd $(autojump $argv)
 end
+
+function n --description "Text editor, with my touch"
+	if $argv=*.sh then 
+		touch $argv && chmod +x $argv && nvim $argv
+	else
+		nvim $argv
+	end
+end
