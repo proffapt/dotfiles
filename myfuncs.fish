4f4703a5d04a84835d2926bc035d9012e84d95f4
proffapt <proffapt@pm.me>
2022-10-10 13:24:40 +0530

    update

--- myfuncs.fish
+++ myfuncs.fish
@@ -8,8 +8,10 @@ end
 
 function n --description "Text editor, with my touch"
 	if test \( $argv = "*.sh" \)
+		echo "sh"
 		touch $argv && chmod +x $argv && nvim $argv
 	else
+		echo "nvim"
 		nvim $argv
 	end
 end
