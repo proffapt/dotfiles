0d2d142c3ae6fa63366c7d735ffe8b0ecf164b3d
proffapt <proffapt@pm.me>
2022-11-07 14:11:51 +0530

    update

---
+++ finicky/.finicky.js
@@ -0,0 +1,39 @@
+// ~/.finicky.js
+
+module.exports = {
+  defaultBrowser: "Firefox",
+  rewrite: [
+    {
+      // Redirect all urls to use https
+      match: ({ url }) => url.protocol === "http",
+      url: { protocol: "https" }
+    }
+  ],      
+  handlers: [
+	{
+	  // Redirecting to spotify app
+	  match: ["*spotify.com*"],
+	  browser: "/Users/proffapt/Applications/Spotify.app"
+	},
+	{
+	  // Redirecting to zoom app
+	  match: ["*zoom.us*"],
+	  browser: "/Applications/zoom.us.app"
+	},
+	{
+	  // Redirecting to Telegram app
+	  match: ["*t.me*"],
+	  browser: "/Applications/Telegram.app"
+	},
+	{
+	  // Redirecting to Discord app
+	  match: ["*discord.gg*"],
+	  browser: "/Applications/Discord.app"
+	}, 
+	{
+	  // Redirecting to Slack app
+	  match: ["*kossiitkgp.slack.com*"],
+	  browser: "/Applications/Slack.app"
+	}
+  ]
+};
