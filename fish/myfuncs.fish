a04af15d75918eda270bc1117ccb1419b87fa0eb
proffapt <proffapt@pm.me>
2022-12-11 10:22:12 +0530

    update

--- fish/myfuncs.fish
+++ fish/myfuncs.fish
@@ -5,3 +5,9 @@ end
 function gt --description "Moving to directories seamlesly"
 	cd $(autojump $argv)
 end
+
+function bts --description "Easily pushing bodhitree session updates to github"
+	git add .
+	git commit -m "session $argv"
+	git push origin main
+end
