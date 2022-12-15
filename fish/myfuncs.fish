0d7615b680dbef7015ab5ac19832c0c5923596fd
proffapt <proffapt@pm.me>
2022-12-15 20:44:26 +0530

    update

--- fish/myfuncs.fish
+++ fish/myfuncs.fish
@@ -15,3 +15,7 @@ end
 function asi --description "AppleStoreInstall - install software from apple store"
 	mas install $(mas search $argv | grep -i "$argv" | fzf | awk '{print $1}')
 end
+
+function asu --description "AppleStoreUninstall - uninstall software from apple store"
+	mas uninstall $(mas search $argv | grep -i "$argv" | fzf | awk '{print $1}')
+end
