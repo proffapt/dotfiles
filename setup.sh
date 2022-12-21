05ca6c80f005a645fda34de797ae78217c7e35b0
proffapt <proffapt@pm.me>
2022-12-21 12:13:03 +0530

    update

--- setup.sh
+++ setup.sh
@@ -20,7 +20,7 @@ brew upgrade
 
 echo "Login into apple account and configure settings now, by then imma install some stuff"
 ## Installing casks from brew 
-brew install --cask veracrypt discord keepassxc firefox telegram obsidian warp keycastr element finicky bartender docker aldente vlc eiskaltdcpp android-file-transfer mpv slack vscode
+brew install --cask veracrypt discord keepassxc firefox telegram obsidian warp keycastr element finicky bartender docker aldente vlc eiskaltdcpp android-file-transfer mpv slack vscodium
 
 ## Installing formulae from brew
 brew install git docker wget curl neovim bash gh fswatch mas fish bat npm neofetch htop thefuck
