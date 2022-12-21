2dd6ee54f57e4c9f3f76011cf2f903f2ad879bee
proffapt <proffapt@pm.me>
2022-12-21 12:17:18 +0530

    update

--- setup.sh
+++ setup.sh
@@ -20,12 +20,13 @@ brew upgrade
 
 echo "Login into apple account and configure settings now, by then imma install some stuff"
 ## Installing casks from brew 
-brew install --cask veracrypt discord keepassxc firefox telegram obsidian warp keycastr element finicky bartender docker aldente vlc eiskaltdcpp android-file-transfer mpv slack vscodium
+brew install --cask veracrypt discord keepassxc firefox telegram obsidian warp keycastr element finicky bartender docker aldente vlc eiskaltdcpp android-file-transfer mpv slack
 
 ## Installing formulae from brew
 brew install git docker wget curl neovim bash gh fswatch mas fish bat npm neofetch htop thefuck
 sudo npm install yarn -g
 npm i -g who-unfollowed-me
+
 fish
 
 ## installing from apple store
